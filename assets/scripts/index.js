b08cde15fd2a2a64aee815c37d6b57efd151f9bc
Elbay Malik <eleo.666@gmail.com>
2023-05-23 22:40:40 +0200

    Update index.js
    Carousel to change image every 3 seconds

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,26 @@
+let recipes = [
+  {
+    title: 'Recipe 1',
+    image: '../images.Food01.jpg',
+    description: 'This is recipe 1'
+  },
+  {
+    title: 'Recipe 2',
+    image: '../images.Food01.jpg',
+    description: 'This is recipe 2'
+  },
+];
+
+let currentRecipe = 0;
+
+setInterval(() => {
+  document.getElementById('carousel-image').src = recipes[currentRecipe].image;
+  document.getElementById('carousel-title').innerText = recipes[currentRecipe].title;
+  document.getElementById('carousel-description').innerText = recipes[currentRecipe].description;
+
+  currentRecipe = (currentRecipe + 1) % recipes.length;
+}, 3000);
+
 // Select the search form and the search input field
 const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('#recipe-search');
